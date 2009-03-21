738ddfc3c6f93e742d3f3d638f8275293ef9c5a7
Jason Smith <jhs@proven-corporation.com>
2009-03-21 15:05:37 +0700

    A basic extension that actually works

--- content/overlay.js
+++ content/overlay.js
@@ -1,9 +1,12 @@
-var markdown = {
-  onLoad: function() {
+var Markdown = {
+  init: function(ev) {
     // initialization code
+    dump('Markdown initializing\n');
+
     this.initialized = true;
     this.strings = document.getElementById("markdown-strings");
   },
+
   onMenuItemCommand: function(e) {
     var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
                                   .getService(Components.interfaces.nsIPromptService);
@@ -11,5 +14,11 @@ var markdown = {
                                 this.strings.getString("helloMessage"));
   },
 
+  onLoad: function() {
+    // According to the bordercolors extension, onload doesn't work so do what they do.
+    var composer = document.getElementById('msgcomposeWindow');
+    composer.addEventListener('focus', function(e) { Markdown.init(e); }, false);
+  },
 };
-window.addEventListener("load", function(e) { markdown.onLoad(e); }, false);
+
+Markdown.onLoad();
