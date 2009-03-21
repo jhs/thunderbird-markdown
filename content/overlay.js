8b97e7ba21b6ba88ffca28f9996d2e92abc53792
Jason Smith <jhs@proven-corporation.com>
2009-03-21 16:15:18 +0700

    Detect modifications to the message body

--- content/overlay.js
+++ content/overlay.js
@@ -18,14 +18,27 @@ var Markdown = {
 
     this.initialized = true;
     this.strings = document.getElementById("markdown-strings");
+
+    var frame = document.getElementById('content-frame');
+    var modified = function(ev) {
+        dump('modified\n');
+    };
+
+    var body = frame.contentDocument.body;
+    var events = ['NodeInserted', 'NodeRemoved', 'CharacterDataModified'];
+    for(var a in events) {
+        body.addEventListener('DOM' + events[a], modified, false);
+    }
   },
 
+  /*
   onMenuItemCommand: function(e) {
     var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
                                   .getService(Components.interfaces.nsIPromptService);
     promptService.alert(window, this.strings.getString("helloMessageTitle"),
                                 this.strings.getString("helloMessage"));
   },
+  */
 
   onLoad: function() {
     // According to the bordercolors extension, onload doesn't work so do what they do.
