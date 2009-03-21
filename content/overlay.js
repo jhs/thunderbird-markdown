e71a8d4f685d0716e8d45b3c92ca14c3eaf17546
Jason Smith <jhs@proven-corporation.com>
2009-03-21 15:37:51 +0700

    Start saving some useful variables

--- content/overlay.js
+++ content/overlay.js
@@ -1,7 +1,20 @@
 var Markdown = {
   init: function(ev) {
     // initialization code
+    if(this.initialized) {
+        dump('Already initialized!\n');
+        return;
+    }
     dump('Markdown initializing\n');
+
+    this.composer = window.gMsgCompose;
+    this.editor   = this.composer.editor;
+    dump('Contents: ' + this.editor.contentsMIMEType + '\n');
+
+    if(this.composer.composeHTML) {
+        dump('Nothing to do for HTML mail\n');
+        return;
+    }
 
     this.initialized = true;
     this.strings = document.getElementById("markdown-strings");
